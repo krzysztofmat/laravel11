a27fc952b6306a52eb50b2ac0a2ac014df359bcd
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-06-10 20:03:54 +0200

    after ep19 - tip 6 - Route Resorces

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'home');
-
-Route::controller(JobController::class)->group(function () {
-    Route::get('/jobs', 'index');
-    Route::get('/jobs/create', 'create');
-    Route::get('/jobs/{job}', 'show');
-    Route::post('/jobs', 'store');
-    Route::get('/jobs/{job}/edit', 'edit');
-    Route::patch('/jobs/{job}', 'update');
-    Route::delete('/jobs/{job}', 'destroy');
-});
-
-
 Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class);
