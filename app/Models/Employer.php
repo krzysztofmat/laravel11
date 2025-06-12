253852200c6118a3bf8cd37b79f713640c811a12
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-06-12 20:04:56 +0200

    after ch23 - step1 - inline authorization

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -4,14 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Employer extends Model
 {
     /** @use HasFactory<\Database\Factories\EmployerFactory> */
     use HasFactory;
 
+    protected $fillable = ['user_id', 'name'];
+
     public function jobs()
     {
         return $this->hasMany(Job::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
     }
 }
