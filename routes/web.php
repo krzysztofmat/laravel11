1ad6772ae2924c4b99079646467b80208e2e6b34
Krzysztof Matysiak <chrismat197966m@gmail.com>
2025-06-12 21:08:41 +0200

    after ch23 - step6 - policies

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::post('/jobs', [JobController::class, 'store'])
 
 Route::get('/jobs/{job}/edit', [JobController::class, 'edit'])
     ->middleware('auth')
-    ->can('edit-job', 'job');
+    ->can('edit', 'job');
 
 Route::patch('/jobs/{job}', [JobController::class, 'update']);
 Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
